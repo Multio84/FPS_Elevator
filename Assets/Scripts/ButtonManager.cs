5ca6bc83564686b8ea4622c02e10d514654174b8
Dmitry Berestennikov <dima.berest@gmail.com>
2025-02-19 18:42:22 +0500

    ButtonManager

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,11 @@
+
+
+public static class ButtonManager
+{
+    public static Button pressedButton;
+
+    public static void TurnOffPressedButton()
+    {
+        pressedButton.TurnOff();
+    }
+}
