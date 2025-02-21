3861415a8c15acd8de573db4067f6b0e06152ac4
Dmitry Berestennikov <dima.berest@gmail.com>
2025-02-22 04:22:58 +0500

    Player attachment fix

--- Assets/Scripts/PlayerElevatorAttachment.cs
+++ Assets/Scripts/PlayerElevatorAttachment.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 public class PlayerElevatorAttachment : MonoBehaviour
 {   
     public Transform player;
-    
+    public Transform elevatorRoot;
+
+    private void Awake()
+    {
+        elevatorRoot = GameObject.Find("ElevatorRoot").transform;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Elevator"))
         {
-            player.transform.SetParent(other.transform);
+            player.transform.SetParent(elevatorRoot);
         }
     }
 
