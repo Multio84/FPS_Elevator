c4ed9c785d531730fbd1541452617ffd1176938b
Dmitry Berestennikov <dima.berest@gmail.com>
2025-03-04 01:06:31 +0500

    Multiple elevators player attachment

--- Assets/Scripts/PlayerElevatorAttachment.cs
+++ Assets/Scripts/PlayerElevatorAttachment.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 public class PlayerElevatorAttachment : MonoBehaviour
 {   
     public Transform player;
-    public Transform elevatorRoot;
-
-
-    void Awake()
-    {
-        Init();
-    }
-
-    public void Init()
-    {
-        elevatorRoot = GameObject.Find("ElevatorRoot").transform;
-    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Elevator"))
         {
-            player.transform.SetParent(elevatorRoot);
+            Transform elevator = other.transform.parent;
+            player.transform.SetParent(elevator);
         }
     }
 
